3872b8d8b66499275a1ef58d8100f01090133129
harichselvam <harichselvamc@gmail.com>
2024-01-12 10:25:06 +0530

    Update preprocess.R

--- preprocess.R
+++ preprocess.R
@@ -42,4 +42,69 @@ lda.class=lda.pred$class
 lda.class
 
 
-table(lda.class,df$Sold)
+table(lda.class,df$Sold )
+
+
+ sum(lda.pred$posterior[,1]>0.8)
+
+
+
+qda.fit=qda(Sold~.,data=df)
+
+qda.fit
+
+qda.pred=predict(qda.fit,df)
+
+qda.pred$posterior
+
+qda.class=qda.pred$class
+qda.class
+
+
+table(qda.class,df$Sold )
+
+
+sum(qda.pred$posterior[,1]>0.8)
+
+
+
+
+
+
+#kmeans
+
+#install.packages("caTools")
+library("caTools")
+set.seed(0)
+split=sample.split(df,SplitRatio = 0.8)
+train_set=subset(df,split==TRUE)
+test_set=subset(df,split==FALSE)
+
+train.fit=glm(Sold~.,data=train_set,family=binomial)
+test.probs=predict(train.fit,test_set,type="response")
+
+test.pred=rep("NO",120)
+test.pred[test.probs>0.5]="YES"
+table(test.pred,test_set$Sold)
+
+
+#install.packages("class")
+library("class")
+trainx=train_set[,-16]
+testx=test_set[,-16]
+
+trainy=train_set$Sold
+testy=test_set$Sold
+
+
+k=3
+
+trainx_s=scale(trainx)
+testx_s=scale(testx)
+
+
+set.seed(0)
+
+
+knn.pred=knn(trainx_s,testx_s,trainy,k=k)
+table(knn.pred,testy)
